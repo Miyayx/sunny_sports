88cb7a3146e3301ad0ed01a521ffc4f986023952
Miyayx <yangxue00.yxmn@gmail.com>
2014-10-24 08:41:52 +0800

    elist的点击无反应问题也解决了，虽然完全不知道发生了什么

--- sunny_sports/static/custom/admin-elist.js
+++ sunny_sports/static/custom/admin-elist.js
@@ -1,34 +1,33 @@
-$(document).ready(function() {
+$(function() {
 
-	var elistInit = function() {
-		$("li").click(function() {
-			var e = $(this).attr('data-id');
-			var url;
-			switch ($(this).parent().attr('id')) {
-			case "person-elist":
-				url = "admin/score_event_person";
-				break;
-			case "group-elist":
-				url = "admin/score_event_group";
-				break;
-			case "group":
-				url = "admin/score_group";
-				break;
-			default:
-				return false;
-			}
+  $(".dd-item").click(function() {
+    var e = $(this).attr('data-id');
+    var url;
+    var id_ = $(this).parent().attr('id'); 
+    console.log(id_);
+    switch (id_) {
+      case "person-elist":
+        url = "admin/score_event_person";
+        break;
+      case "group-elist":
+        url = "admin/score_event_group";
+        break;
+      case "group":
+        url = "admin/score_group";
+        break;
+      default:
+        return false;
+    }
 
-			window.location.href = "#page/" + url + "?event=" + e;
+    window.location.href = "#page/" + url + "?event=" + e;
 
-			//$.ajax({
-			//  url:url+"?event="+e
-			//  }).done(function(data){
-			//    console.log(url);
-			//    $('.page-content-area').html(data);
-			//    window.location.href="#page/"+url;
-			//    });
-		});
-	}
-	elistInit();
+    //$.ajax({
+    //  url:url+"?event="+e
+    //  }).done(function(data){
+    //    console.log(url);
+    //    $('.page-content-area').html(data);
+    //    window.location.href="#page/"+url;
+    //    });
+  });
 });
 
