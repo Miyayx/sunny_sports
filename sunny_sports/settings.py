fd6b0cc7fceed4cebff57daf79e028a73125ac2c
gll1106 <gaolianglillian@foxmail.com>
2014-11-04 19:12:34 +0800

    个人中心更新

--- sunny_sports/settings.py
+++ sunny_sports/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'sunny_sports.wsgi.application'
 DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.mysql',
-        'ENGINE': 'mysql.connector.django',
+        #'ENGINE': 'mysql.connector.django',
         'NAME': 'sunny_sports',
         'USER': 'root',
         'PASSWORD': '123',
