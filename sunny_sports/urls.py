ce355db8edc9f2cdc233f943f01731b9d3752127
Miyayx <yangxue00.yxmn@gmail.com>
2014-12-14 14:20:40 +0800

    set empty page for student, judge, club, committee

--- sunny_sports/urls.py
+++ sunny_sports/urls.py
@@ -1,6 +1,7 @@
 #-*-coding:utf-8-*-
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 import os
 
@@ -8,20 +9,18 @@ urlpatterns = patterns('',
         # Examples:
         url(r'^$', 'sunny_sports.views.login', name='login'),
         url(r'^favicon.ico$', 'sunny_sports.views.home'),
-        url(r'^student/', direct_to_template, {'template':'student/empty.html'}),
-        url(r'^judge/', direct_to_template, {'template':'judge/empty.html'}),
-        url(r'^club/', direct_to_template, {'template':'club/empty.html'}),
-        url(r'^group/', direct_to_template, {'template':'group/empty.html'}),
-        url(r'^committee/', direct_to_template, {'template':'committee/empty.html'}),
-        url(r'^coach/', include('coach/urls')),
-        url(r'^centre/', include('centre/urls')),
-        url(r'^coach_org/', include('coach_org/urls')),
+        url(r'^student/', TemplateView.as_view(template_name='student/empty.html')),
+        url(r'^judge/', TemplateView.as_view(template_name='judge/empty.html')),
+        url(r'^club/', TemplateView.as_view(template_name='club/empty.html')),
+        url(r'^team/', TemplateView.as_view(template_name='team/empty.html')),
+        url(r'^committee/', TemplateView.as_view(template_name='committee/empty.html')),
+        url(r'^coach$', TemplateView.as_view(template_name='coach/home.html')),
+        url(r'^centre$', TemplateView.as_view(template_name='centre/home.html')),
+        url(r'^coach_org$', TemplateView.as_view(template_name='coach_org/home.html')),
 
         #url(r'^blog/', include('blog.urls')),
 
-        url(r'^admin_/', include('admin_.urls')),
-
-        url(r'^static/(?P<path>.*)$)','django.views.static.serve',{'document_root':os.path.join(os.path.dirname(__file__),'static')}),
+        url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root':os.path.join(os.path.dirname(__file__),'static')}),
 
         url(r'^admin/', include(admin.site.urls)),
         )
