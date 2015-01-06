d3197386a212cd418b965b3b6f5d0e0b1fb44dbe
Miyayx <yangxue00.yxmn@gmail.com>
2015-01-06 20:24:55 +0800

    import data shell

--- data.sh
+++ data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python manage.py dumpdata --indent 2 --format json > data.json
-python manage.py sqlclear
+#python manage.py dumpdata sp --indent 2 --format json > data.json
+python manage.py sqlclear sp| python manage.py dbshell
 python manage.py syncdb
 python manage.py loaddata data.json
 
