9b23612af1ce6470be9deac535e12edc3da8c6fe
HKHome <ryk1990@bupt.edu.cn>
2015-01-30 23:59:35 +0800

    hhhh

--- sunny_sports/sp/coach_views.py
+++ sunny_sports/sp/coach_views.py
@@ -24,12 +24,25 @@ def train(req):
     # 用这个id查信息哦
     print uuid
     coach = Coach.objects.filter(property__user_id=uuid)
-    #if coach[0].p_level = 0:
-     #   train = Train.objects.filter()
-    #else if 
-        
     
-    return render_to_response('coach/train.html',{"coach":coach[0], "train":train})
+    if coach[0].p_level == 0:
+        ltrain = Train.objects.filter(level=1)
+        return render_to_response('coach/train.html',{"coach":coach[0], "ltrain":ltrain})
+    elif coach[0].p_level == 1:
+        ltrain = CoachTrain.objects.filter(coach_id=coach[0].id, train__level=1)
+        #ltrain = Train.objects.filter(id=ctrain[0].train_id)
+        mtrain = Train.objects.filter(level=2)
+        return render_to_response('coach/train.html',{"coach":coach[0], "ltrain":ltrain[0], "mtrain":mtrain})
+    elif coach[0].p_level == 2:
+        ltrain = CoachTrain.objects.filter(coach_id=coach[0].id, train__level=1)
+        mtrain = CoachTrain.objects.filter(coach_id=coach[0].id, train__level=2)
+        htrain = Train.objects.filter(level=3)
+        return render_to_response('coach/train.html',{"coach":coach[0], "ltrain":ltrain[0], "mtrain":mtrain[0], "htrain":htrain})
+    else:
+        ltrain = CoachTrain.objects.filter(coach_id=coach[0].id, train__level=1)
+        mtrain = CoachTrain.objects.filter(coach_id=coach[0].id, train__level=2)
+        htrain = CoachTrain.objects.filter(coach_id=coach[0].id, train__level=3)
+        return render_to_response('coach/train.html',{"coach":coach[0], "ltrain":ltrain[0], "mtrain":mtrain[0], "htrain":htrain[0]})
 
 def center(req):
     uuid = req.session.get('uuid',0)
