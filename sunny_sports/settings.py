a85f251997ea1cc13b67161fb335fa2282588e81
Miyayx <yangxue00.yxmn@gmail.com>
2015-02-25 21:49:23 +0800

    add static root

--- sunny_sports/settings.py
+++ sunny_sports/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=( 
         os.path.join(os.path.dirname(__file__), 'static').replace('\\','/'), 
         ) 
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
