bee81c37d4eaf566fd10778ec7146631dccfef5e
Miyayx <yangxue00.yxmn@gmail.com>
2015-03-01 13:16:26 +0800

    center save problem

--- sunny_sports/static/custom/validate.js
+++ sunny_sports/static/custom/validate.js
@@ -3,31 +3,31 @@ function isEmail(email) {
     return regex.test(email);
 }
 
-jQuery.extend(jQuery.validator.messages, {
-    required: "必填",
-    remote: "请修正该字段",
-    email: "请输入正确格式的电子邮件",
-    url: "请输入合法的网址",
-    date: "请输入合法的日期",
-    dateISO: "请输入合法的日期 (ISO).",
-    number: "请输入合法的数字",
-    digits: "只能输入整数",
-    creditcard: "请输入合法的信用卡号",
-    equalTo: "请再次输入相同的值",
-    accept: "请输入拥有合法后缀名的字符串",
-    maxlength: jQuery.validator.format("请输入一个长度最多是 {0} 的字符串"),
-    minlength: jQuery.validator.format("请输入一个长度最少是 {0} 的字符串"),
-    rangelength: jQuery.validator.format("请输入一个长度介于 {0} 和 {1} 之间的字符串"),
-    range: jQuery.validator.format("请输入一个介于 {0} 和 {1} 之间的值"),
-    max: jQuery.validator.format("请输入一个最大为 {0} 的值"),
-    min: jQuery.validator.format("请输入一个最小为 {0} 的值")
-});
+//jQuery.extend(jQuery.validator.messages, {
+//    required: "必填",
+//    remote: "请修正该字段",
+//    email: "请输入正确格式的电子邮件",
+//    url: "请输入合法的网址",
+//    date: "请输入合法的日期",
+//    dateISO: "请输入合法的日期 (ISO).",
+//    number: "请输入合法的数字",
+//    digits: "只能输入整数",
+//    creditcard: "请输入合法的信用卡号",
+//    equalTo: "请再次输入相同的值",
+//    accept: "请输入拥有合法后缀名的字符串",
+//    maxlength: jQuery.validator.format("请输入一个长度最多是 {0} 的字符串"),
+//    minlength: jQuery.validator.format("请输入一个长度最少是 {0} 的字符串"),
+//    rangelength: jQuery.validator.format("请输入一个长度介于 {0} 和 {1} 之间的字符串"),
+//    range: jQuery.validator.format("请输入一个介于 {0} 和 {1} 之间的值"),
+//    max: jQuery.validator.format("请输入一个最大为 {0} 的值"),
+//    min: jQuery.validator.format("请输入一个最小为 {0} 的值")
+//});
 
-$(document).ready(function () {
-    jQuery.validator.addMethod("isIdCardNo", function (value, element) {
-        return this.optional(element) || isIdCardNo(value);
-    }, "请正确输入您的身份证号码");
-});//增加身份证验证
+//$(document).ready(function () {
+//    jQuery.validator.addMethod("isIdCardNo", function (value, element) {
+//        return this.optional(element) || isIdCardNo(value);
+//    }, "请正确输入您的身份证号码");
+//});//增加身份证验证
 
 function isIdCardNo(num) {
     var factorArr = new Array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2, 1);
