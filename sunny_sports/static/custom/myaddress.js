c7478993df1ee8bd33bff399b87fad2fb96357a9
Miyayx <yangxue00.yxmn@gmail.com>
2015-03-01 19:51:33 +0800

    train publish信息的正确性与必须性判断, 报名时信息格式的完整性判断与格式判断

--- sunny_sports/static/custom/myaddress.js
+++ sunny_sports/static/custom/myaddress.js
@@ -1,60 +1,75 @@
 // load city data
 var citylist;
 $.getJSON('../../static/custom/city.json', function(data) {
-	citylist = data;
+    citylist = data;
 });
 $('#province').editable({
-	name: 'province',
-	source: function() {
-		a = Array();
-		Object.keys(citylist).forEach(function(v) {
-			a.push({
-				text: v,
-				value: v
-			});
-		});
-		return a;
-	}
+    name: 'province',
+    validate: function(v) {
+        if ($.trim(v) == '') {
+            return '必填项';
+        }
+    },
+    source: function() {
+        a = Array();
+        Object.keys(citylist).forEach(function(v) {
+            a.push({
+                text: v,
+                value: v
+            });
+        });
+        return a;
+    }
 });
 $('#city').editable({
-	name: 'city',
-	source: function() {
-		var k = $('#province').html().trim();
-		if (k != "Empty") {
-			var a = Array();
-			Object.keys(citylist[k]).forEach(function(v, i) {
-				a.push({
-					value: v,
-					text: v
-				});
-			});
-			return a;
-		} else {
-			return {}
-		}
-	}
+    name: 'city',
+    validate: function(v) {
+        if ($.trim(v) == '') {
+            return '必填项';
+        }
+    },
+    source: function() {
+        var k = $('#province').html().trim();
+        if (k != "Empty") {
+            var a = Array();
+            Object.keys(citylist[k]).forEach(function(v, i) {
+                a.push({
+                    value: v,
+                    text: v
+                });
+            });
+            return a;
+        } else {
+            return {}
+        }
+    }
 });
 $('#dist').editable({
-	name: 'dist',
-	source: function() {
-		var k = $('#province').html().trim();
-		var j = $('#city').html().trim();
-		if (k != "Empty" && j != "Empty") {
-			var a = Array();
-			citylist[k][j].forEach(function(v, i) {
-				a.push({
-					value: v,
-					text: v
-				});
-			});
-			return a;
-		} else {
-			return {}
-		}
-	}
+    name: 'dist',
+    validate: function(v) {
+        if ($.trim(v) == '') {
+            return '必填项';
+        }
+    },
+    source: function() {
+        var k = $('#province').html().trim();
+        var j = $('#city').html().trim();
+        if (k != "Empty" && j != "Empty") {
+            var a = Array();
+            citylist[k][j].forEach(function(v, i) {
+                a.push({
+                    value: v,
+                    text: v
+                });
+            });
+            return a;
+        } else {
+            return {}
+        }
+    }
 });
 $('#address').editable({
-	type: 'text',
-	name: 'address'
+    type: 'text',
+    name: 'address'
 });
 
