0f09c8427c5c7f346d4f55085841066be4aaf2aa
Miyayx <yangxue00.yxmn@gmail.com>
2015-03-08 14:11:54 +0800

    x-editable 的省市联动问题

--- sunny_sports/static/custom/myaddress.js
+++ sunny_sports/static/custom/myaddress.js
@@ -1,75 +1,87 @@
 // load city data
 var citylist;
 $.getJSON('../../static/custom/city.json', function(data) {
-    citylist = data;
+	citylist = data;
 });
 $('#province').editable({
-    name: 'province',
-    //validate: function(v) {
-    //    if ($.trim(v) == '') {
-    //        return '必填项';
-    //    }
-    //},
-    source: function() {
-        var a = Array();
-        for(var v in citylist){
-            a.push({
-                text: v,
-                value: v
-            });
-        };
-        return a;
-    }
-});
-$('#city').editable({
-    name: 'city',
-    //validate: function(v) {
-    //    if ($.trim(v) == '') {
-    //        return '必填项';
-    //    }
-    //},
-    source: function() {
-        var k = $.trim($('#province').html());
-        if (k != "Empty") {
-            var a = Array();
-            for(var v in citylist[k]){
-                a.push({
-                    value: v,
-                    text: v
-                });
-            };
-            return a;
-        } else {
-            return {}
+	name: 'province',
+	//validate: function(v) {
+	//    if ($.trim(v) == '') {
+	//        return '必填项';
+	//    }
+	//},
+	source: function() {
+		var a = Array();
+		for (var v in citylist) {
+			a.push({
+				text: v,
+				value: v
+			});
+		};
+		return a;
+	},
+	validate: function(v) {
+		v = $.trim(v);
+		if (! ($(this).text() == v || $(this)[0].value == v)){ 
+        $('#city').editable('setValue', "");
+        $('#dist').editable('setValue', "");
         }
-    }
+	}
+});
+
+$('#city').editable({
+	name: 'city',
+	//validate: function(v) {
+	//    if ($.trim(v) == '') {
+	//        return '必填项';
+	//    }
+	//},
+	source: function() {
+		var k = $.trim($('#province').html());
+		if (k != "Empty") {
+			var a = Array();
+			for (var v in citylist[k]) {
+				a.push({
+					value: v,
+					text: v
+				});
+			};
+			return a;
+		} else {
+			return {}
+		}
+	},
+	validate: function(v) {
+		v = $.trim(v);
+		if (! ($(this).text() == v || $(this)[0].value == v)) $('#dist').editable('setValue', "");
+	}
 });
 $('#dist').editable({
-    name: 'dist',
-    //validate: function(v) {
-    //    if ($.trim(v) == '') {
-    //        return '必填项';
-    //    }
-    //},
-    source: function() {
-        var k = $.trim($('#province').html());
-        var j = $.trim($('#city').html());
-        if (k != "Empty" && j != "Empty") {
-            var a = Array();
-            for(var v in citylist[k][j]){
-                a.push({
-                    value: citylist[k][j][v],
-                    text: citylist[k][j][v]
-                });
-            };
-            return a;
-        } else {
-            return {}
-        }
-    }
+	name: 'dist',
+	//validate: function(v) {
+	//    if ($.trim(v) == '') {
+	//        return '必填项';
+	//    }
+	//},
+	source: function() {
+		var k = $.trim($('#province').html());
+		var j = $.trim($('#city').html());
+		if (k != "Empty" && j != "Empty") {
+			var a = Array();
+			for (var v in citylist[k][j]) {
+				a.push({
+					value: citylist[k][j][v],
+					text: citylist[k][j][v]
+				});
+			};
+			return a;
+		} else {
+			return {}
+		}
+	}
 });
 $('#address').editable({
-    type: 'text',
-    name: 'address'
+	type: 'text',
+	name: 'address'
 });
 
