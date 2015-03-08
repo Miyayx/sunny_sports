1f61a50e47908ccf5b77deb9c5f41b5d9081aa34
kltc <kltc@localhost.localdomain>
2015-03-08 19:54:50 +0800

    data backup script modify

--- databackup.sh
+++ databackup.sh
@@ -2,18 +2,20 @@
 
 projectpath='/home/kltc/Project/sunny_sports/sunny_sports/'
 backuppath='/home/kltc/kuaileticao/'
-database='sunny_sports'
+#database='sunny_sports'
+database='kuaileticao'
 
-datename=$(date+%Y%m%d%H%M%S) #文件名按时间命名
+datename=$(date +%Y%m%d%H%M%S) #文件名按时间命名
 mkdir /home/kltc/kuaileticao/
-mkdir /home/kltc/kuaileticao/mediabackup
-mkdir /home/kltc/kuaileticao/sqlbackup
+mkdir /home/kltc/kuaileticao/mediabackup/
+mkdir /home/kltc/kuaileticao/sqlbackup/
 
 tar zcvf ${backuppath}mediabackup/media.${datename}.tar.gz ${projectpath}media/ #备份压缩网站的图片文件夹 
-sqlbackupname=${backuppath}sqlbakup/mysql.${datename}
-/usr/bin/mysqldump -uqueen -pdorm $database > ${sqlbackupname}.sql #从数据库备份数据
-tar zcxf ${sqlbackupname}.tar.gz ${sqlbackupname}.sql 
+sqlbackupname=${backuppath}sqlbackup/mysql.${datename}
+#/usr/bin/mysqldump -uqueen -pdorm $database > ${sqlbackupname}.sql #从数据库备份数据
+/usr/bin/mysqldump $database > ${sqlbackupname}.sql #从数据库备份数据
+tar zcvf ${sqlbackupname}.tar.gz ${sqlbackupname}.sql 
 rm -rf ${sqlbackupname}.sql
 
 #还原
-#/usr/bin/mysql -uqueen -pdorm $database < db_name.sql
+#/usr/bin/mysql -uqueen -p $database < db_name.sql
