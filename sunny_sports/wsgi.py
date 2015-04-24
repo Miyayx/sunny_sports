0fb7ed73486b605481b1aa03e6463fd7bac84643
kltc <kltc@localhost.localdomain>
2015-04-24 21:09:07 +0800

    uwsgi 编码问题

--- sunny_sports/wsgi.py
+++ sunny_sports/wsgi.py
@@ -7,8 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
+# -*- coding:utf-8 -*-
+
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sunny_sports.settings")
+
+os.environ['LC_ALL']="en_US.UTF-8"
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
