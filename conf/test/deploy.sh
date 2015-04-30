bce643e000c179fe55c900cc8f2ed39c00698cf9
kltc <kltc@localhost.localdomain>
2015-04-30 21:05:40 +0800

    change deploy script

--- conf/test/deploy.sh
+++ conf/test/deploy.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
-cp kuaileticao.miyayx.me /etc/nginx/sites-available/
-ln -s /etc/nginx/sites-available/kuaileticao.miyayx.me /etc/nginx/sites-enabled/
+#cp kuaileticao.miyayx.me /etc/nginx/sites-available/
+#ln -s /etc/nginx/sites-available/kuaileticao.miyayx.me /etc/nginx/sites-enabled/
 
-mkdir /var/log/nginx
-mkdir /var/log/nginx/kuaileticao.miyayx.me
-mkdir /var/log/celery
-mkdir /var/run/celery
-chown -R kltc:kltc /var/log/celery
-chown -R kltc:kltc /var/run/celery
+#mkdir /var/log/nginx
+#mkdir /var/log/nginx/kuaileticao.miyayx.me
+#mkdir /var/log/celery
+#mkdir /var/run/celery
+#chown -R kltc:kltc /var/log/celery
+#chown -R kltc:kltc /var/run/celery
 
-#cp celery_test /etc/sysconfig/
-#cp celerybeat_test /etc/sysconfig/
-#cp celery_test.service /etc/systemd/system/
-#cp celerybeat_test.service /etc/systemd/system/
+rm /etc/sysconfig/celery_test
+rm /etc/sysconfig/celerybeat_test
+rm /etc/systemd/system/celery_test.service
+rm /etc/systemd/system/celerybeat_test.service
+ln -s /home/kltc/Project/sunny_sports/conf/test/celery_test /etc/sysconfig/
+ln -s /home/kltc/Project/sunny_sports/conf/test/celerybeat_test /etc/sysconfig/
+ln -s /home/kltc/Project/sunny_sports/conf/test/celery_test.service /etc/systemd/system/
+ln -s /home/kltc/Project/sunny_sports/conf/test/celerybeat_test.service /etc/systemd/system/
 
-systemctl daemon-reload
-systemctl restart nginx
+#systemctl daemon-reload
+#systemctl restart nginx
 #systemctl restart celery_test
 #systemctl enable celery_test
 #systemctl restart celerybeat_test
