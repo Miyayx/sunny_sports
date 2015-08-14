13ccc7eeecc8468e3e91dcaa4793d3edc0397323
Miyayx <yangxue00.yxmn@gmail.com>
2015-08-14 22:11:51 +0800

    add acks_late

--- game/tasks.py
+++ game/tasks.py
@@ -12,27 +12,27 @@ from django.utils import timezone
 
 from sunny_sports import settings
 
-@task
+@task(acks_late=True)
 def game_reg_start(g_id):
     print("%s game reg start at %s"%(g_id, timezone.now()))
     Game.objects.filter(id=g_id, pass_status=1, reg_status=0, reg_stime__lte=timezone.now()).update(reg_status=1)
 
-@task
+@task(acks_late=True)
 def game_reg_end(g_id):
     print("%s game reg end at %s"%(g_id, timezone.now()))
     Game.objects.filter(id=g_id, pass_status=1, reg_status__lte=2, reg_etime__lte=timezone.now()).update(reg_status=2)
 
-@task
+@task(acks_late=True)
 def game_start(g_id):
     print("%s game start at %s"%(g_id, timezone.now()))
     Game.objects.filter(id=g_id, pass_status=1, game_stime__lte=timezone.now()).update(game_status=1)
 
-@task
+@task(acks_late=True)
 def game_end(g_id):
     print("%s game end at %s"%(g_id, timezone.now()))
     Game.objects.filter(id=g_id, pass_status=1, game_etime__lte=timezone.now()).update(game_status=2)
 
-@task
+@task(acks_late=True)
 def payment_check(t_id):
     #规定时间进行检查，若还未缴费,删除报名记录
     # 若缴费前报名已经截止？
--- sp/tasks.py
+++ sp/tasks.py
@@ -43,27 +43,27 @@ def train_end_p():
     print("%d trains end at %s"%(len(trains), timezone.now()))
     trains.update(train_status=2)
 
-@task
+@task(acks_late=True)
 def train_reg_start(t_id):
     print("%s train reg start at %s"%(t_id, timezone.now()))
     Train.objects.filter(id=t_id, pass_status=1, reg_status=0, reg_stime__lte=timezone.now()).update(reg_status=1)
 
-@task
+@task(acks_late=True)
 def train_reg_end(t_id):
     print("%s train reg end at %s"%(t_id, timezone.now()))
     Train.objects.filter(id=t_id, pass_status=1, reg_status__lte=2, reg_etime__lte=timezone.now()).update(reg_status=2)
 
-@task
+@task(acks_late=True)
 def train_start(t_id):
     print("%s train start at %s"%(t_id, timezone.now()))
     Train.objects.filter(id=t_id, pass_status=1, train_stime__lte=timezone.now()).update(train_status=1)
 
-@task
+@task(acks_late=True)
 def train_end(t_id):
     print("%s train end at %s"%(t_id, timezone.now()))
     Train.objects.filter(id=t_id, pass_status=1, train_etime__lte=timezone.now()).update(train_status=2)
 
-@task
+@task(acks_late=True)
 def payment_check(ct_id):
     #规定时间进行检查，若还未缴费,删除报名记录
     # 若缴费前报名已经截止？
