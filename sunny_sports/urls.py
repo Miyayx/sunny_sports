6e056b05a4e493477b4ce13db9aee5632386c061
Miyayx <yangxue00.yxmn@gmail.com>
2015-08-28 20:16:19 +0800

    change help url

--- sunny_sports/urls.py
+++ sunny_sports/urls.py
@@ -36,7 +36,7 @@ urlpatterns = patterns('',
         url(r'^get_captcha$','sp.views.get_captcha'),
 
         url(r'^tutorial/coach$', 'sp.help_views.coach_help'),
-        url(r'^tutorial/game/group$', 'sp.help_views.game_group_help'),
+        url(r'^tutorial/team', 'sp.help_views.game_group_help'),
 
         url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root':os.path.join(os.path.dirname(__file__),'static')}),
         #url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
